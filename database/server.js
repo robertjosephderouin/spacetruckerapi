3772fe80fc5c7b96cfeaa8a9b1648a83e7bceb37
Caroline Eubanks <cmeubanks@gmail.com>
2021-06-01 15:25:25 -0600

    api running

--- database/server.js
+++ database/server.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const app = express();
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
 
-app.set('port', process.env.PORT || 5432);
+app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Space Truckers API';
 
-app.get('/', (request, response) => {
-  response.send('Welcome to Space Truckers API, the new Wild West');
+// app.get('/', (request, response) => {
+//   response.send('Oh hey Pet Box');
+// });
+
+app.get('/api/v1/jobs', async (request, response) => {
+  try {
+    const jobs = await database('jobs').select();
+    response.status(200).json(jobs);
+  } catch(error) {
+    response.status(500).json({ error });
+  }
 });
 
 app.listen(app.get('port'), () => {
