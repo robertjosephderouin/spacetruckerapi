3edc7d7b4c9141360f0c21f2cd2643f3742ddaa0
Caroline Eubanks <cmeubanks@gmail.com>
2021-06-02 10:22:33 -0600

    patch not working

--- database/server.js
+++ database/server.js
@@ -12,18 +12,27 @@ app.get('/api/v1/jobs', async (request, response) => {
     const jobs = await database('jobs').select();
     response.status(200).json(jobs);
   } catch(error) {
-    response.status(500).json({ error });
+    response.status(500).json({ error: error.message });
   }
 });
 
 app.delete('/api/v1/jobs/:id', async (request, response) => {
  try {
   const selectedJob = await database('jobs').where( {id: request.params.id }).del();
-  response.status(200).json({ success: true})
+  response.status(200).json({ success: true })
   } catch(error) {
-  response.status(500).json({ error });
+  response.status(500).json({ error: error.message });
   }
 });
+
+app.patch('/api/v1/jobs/:id', async (request, response) => {
+  try {
+    const modifyJob = await database('jobs').where( {id: request.params.id }).update(request.body).returning('*');
+    response.status(200).json({ success: true })
+  } catch(error) {
+    response.status(500).json({ error: error.message });
+  }
+})
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`);
