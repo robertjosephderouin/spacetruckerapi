ceb2b164de349fc8d7f00d943eed5d22b16db701
Caroline Eubanks <cmeubanks@gmail.com>
2021-06-03 11:54:13 -0600

    added new migration

--- jobsData.js
+++ jobsData.js
@@ -1,6 +1,5 @@
 let jobsData = [
   {
-    id: 1,
     image: '../../../public/mars.jpeg',
     name: 'Mars',
     date: '11:10:2195',
@@ -9,7 +8,6 @@ let jobsData = [
     fluff: 'Mars is a barren planet with little resources after it was used up by hyper capitalists in the early 21st century.',
   },
   {
-    id: 2,
     image: '../public/jupiter.jpeg',
     name: 'Jupiter',
     date: '6:05:2198',
@@ -18,7 +16,6 @@ let jobsData = [
     fluff: 'Jupiter is a big gas giant and you won\'t be able to land your rig on the surface.',
   },
   {
-    id: 3,
     image: 'path.dummy',
     name: 'Pluto',
     date: '01:21:2234',
@@ -27,7 +24,6 @@ let jobsData = [
     fluff: 'Pluto is now a planet again and thus we can space truck too it.  Be careful its too small to land on normally so you have to space parallel park.',
   },
   {
-    id: 4,
     image: 'path.dummy',
     name: 'LV-426',
     date: '04:22:2179',
